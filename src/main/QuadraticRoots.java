e6355df611b6d169207a27dedd0c110557d0e846
Your Name <ptsvyatkov97@gmail.com>
2023-07-29 13:21:50 +0200

    quadratic roots

---
+++ src/main/QuadraticRoots.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class QuadraticRoots {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int a,b,c;
+        double r1,r2;
+        System.out.println("Enter a,b,c");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        double sqrt1 = Math.sqrt(b * b - 4 * a * c);
+        r1=(-b+ sqrt1)/(2*a);
+        r2=(-b- sqrt1)/(2*a);
+
+        System.out.println("The roots of the quadratic expression are "+r1 +" and "+ r2);
+    }
+}
