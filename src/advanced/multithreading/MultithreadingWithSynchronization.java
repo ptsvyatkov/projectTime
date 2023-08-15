b4cebc1da9eb021861d2cab3dd774430674433b8
Pavel Tsvyatkov <ptsvyatkov97@gmail.com>
2023-08-15 15:26:07 +0200

    Daemon, multithreading with and without synchronization

--- src/advanced/multithreading/MultithreadingWithSynchronization.java
+++ src/advanced/multithreading/MultithreadingWithSynchronization.java
@@ -5,8 +5,8 @@ class MyData2 {
     synchronized void display(String str) {             // synchronized method -> now no thread can enter if already in use
     //        synchronized (this) {                     // synchronize block
             for (int i = 0; i < str.length(); i++) {    /*
-                System.out.println(str.charAt(i));      Critical section
-            }                                                           /*
+                System.out.println(str.charAt(i));      Critical section */
+            }
         }
     // }
 }
