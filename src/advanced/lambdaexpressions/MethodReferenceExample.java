244974d02fbb4d32010c69ee17e5d3e952365967
Pavel Tsvyatkov <ptsvyatkov97@gmail.com>
2023-08-18 17:32:49 +0200

    added comments and examples

--- src/advanced/lambdaexpressions/MethodReferenceExample.java
+++ src/advanced/lambdaexpressions/MethodReferenceExample.java
@@ -1,31 +1,36 @@
 package lambdaexpressions;
 
 interface MethodReference {
-    //void display(String str);
-    int display(String str, String str2);
+    void display(String str);
+    //int display(String str, String str2);
 }
 
 public class MethodReferenceExample {
 
-    public MethodReferenceExample(String s){
+    public MethodReferenceExample(String s) {
         System.out.println(s.toUpperCase());
     }
 
-    void reverse(String str){
+    void reverse(String str) {
         StringBuffer sb = new StringBuffer(str);
         sb.reverse();
         System.out.println(sb);
     }
 
     public static void main(String[] args) {
-        // MethodReference mr = System.out::println;
+
+
+        // MethodReference mr = (s) -> System.out.println(s);  This lambda is equal to writing
+        // MethodReference mr = System.out::println;          this line using method reference
+        // mr.display("hello");              It's already known what parameters the method in the functional interface takes.
+
         // MethodReferenceExample mre = new MethodReferenceExample();  // for non-static methods we need to create an object
         // MethodReference mr = mre::reverse;                          // and assign it to the lambda expression
         // mr.display("Hello");
 
         // MethodReference mr = MethodReferenceExample::new; // now it is referring to the constructor
 
-        MethodReference mr2 = String::compareTo;
-        System.out.println(mr2.display("Hello", "Hello"));
+        //MethodReference mr2 = String::compareTo;
+        //System.out.println(mr2.display("Hello", "Hello"));
     }
 }
