402a6b0c81b9186ce3d3567df10b335acb751afd
Pavel Tsvyatkov <ptsvyatkov97@gmail.com>
2023-08-20 17:16:50 +0200

    FileOutputStream writing

---
+++ src/advanced/iostreams/FileStreams.java
@@ -0,0 +1,29 @@
+package iostreams;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileStreams {
+
+    public static void main(String[] args) {
+        try {
+            try (FileOutputStream fos = new FileOutputStream("./src/advanced/iostreams/Test.txt");) {
+
+                String str = "file output stream";
+                byte[] b = str.getBytes();
+                // fos.write(str.getBytes());           Example#1
+
+
+                // for (byte el : b) {
+                //    fos.write(el);
+                // }
+
+                // fos.write(b,0,str.length()-6);
+
+                fos.write(b);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    };
+}
