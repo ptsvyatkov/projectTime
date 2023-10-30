b0a2261701515b35f4700cecb6ad891b0911fdbb
Pavel Tsvyatkov <ptsvyatkov97@gmail.com>
2023-10-30 16:03:18 +0100

    Tree Map Example

--- src/advanced/collectionframework/TreeMapExample.java
+++ src/advanced/collectionframework/TreeMapExample.java
@@ -1,2 +1,14 @@
-package collectionframework;public class TreeMapExample {
+package collectionframework;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> tm = new TreeMap<>(Map.of(0,"A", 1, "B", 2, "C", 3, "D"));
+        tm.put(4,"E");
+        tm.put(5,"G");
+        System.out.println(tm);
+        System.out.println(tm.ceilingEntry(5).getValue());
+    }
 }
